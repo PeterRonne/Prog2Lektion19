7e2fc10140ec9fc2c60f06e6f607802c9204e43b
Peter Roest Rønne <77733864+PeterRonne@users.noreply.github.com>
2024-04-15 11:19:12 +0200

    opgave 1

--- src/opgave01/Opgave01.java
+++ src/opgave01/Opgave01.java
@@ -1,12 +1,16 @@
 package opgave01;
 
 import opgave01.models.Troll;
+import opgave01.models.weaponBehaviors.KnifeBehavior;
+import opgave01.models.weaponBehaviors.WeaponBehavior;
 
 public class Opgave01 {
     public static void main(String[] args) {
         Troll troll = new Troll();
 
         troll.fight();
+
+        troll.setWeapon(new KnifeBehavior());
 
         troll.fight();
     }
--- src/opgave01/models/Troll.java
+++ src/opgave01/models/Troll.java
@@ -1,6 +1,11 @@
 package opgave01.models;
 
+import opgave01.models.weaponBehaviors.AxeBehavior;
+import opgave01.models.weaponBehaviors.KnifeBehavior;
+import opgave01.models.weaponBehaviors.WeaponBehavior;
+
 public class Troll extends Character {
     public Troll() {
+        weapon = new AxeBehavior();
     }
 }
